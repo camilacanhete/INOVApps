9731f4ea651fe3c812e9bc38b9d8b6ac64bbdd5c
camilacanhete <csantos@MacBook-Pro-de-Camila.local>
2014-08-18 18:11:48 -0300

    Versão 0.0.2

---
+++ ProjetoTurismo/Assets/Scripts/ChangePositionScript.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+var speed : float = 4.0;
+var currentMoves : int = 0;
+ 
+private var nav : NavMeshAgent;					// Reference to the nav mesh agent.
+
+private var nextPoint : GameObject = null;	
+private var currentTile : int = 0;
+
+function Start () {
+	nav = GetComponent(NavMeshAgent);
+	
+}
+
+function Update () {
+ 
+ 	nav.speed = speed;
+ 	
+ 	Debug.Log(nav.remainingDistance);
+ 	
+ 	if(currentMoves > 0 && nav.pathStatus == NavMeshPathStatus.PathComplete && nav.remainingDistance == 0)
+ 	{
+ 		nextPoint = GameObject.Find("Tile" + (currentTile + 1));
+
+	 	nav.destination = nextPoint.transform.position;
+	 	
+ 		currentMoves--;
+ 		currentTile++;
+ 	}
+	
+}
