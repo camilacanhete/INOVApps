86c1cff143accf72cfd04aed7570070c7edd50b5
camilacanhete <csantos@MacBook-Pro-de-Camila.local>
2014-08-18 21:47:11 -0300

    Versão 0.0.3

--- ProjetoTurismo/Assets/Scripts/ChangePositionScript.js
+++ ProjetoTurismo/Assets/Scripts/ChangePositionScript.js
@@ -2,14 +2,23 @@
 
 var speed : float = 4.0;
 var currentMoves : int = 0;
+var playerNumber : int = 1;
+var offTileDistance : float = 0.3;
+
+public var playerIsMoving : boolean = false;
  
 private var nav : NavMeshAgent;					// Reference to the nav mesh agent.
-
 private var nextPoint : GameObject = null;	
 private var currentTile : int = 0;
+private var movementDirection : Vector3 = Vector3.zero;
+private var col : CapsuleCollider;
+
+private var gameController : GameObject;
 
 function Start () {
 	nav = GetComponent(NavMeshAgent);
+	col = GetComponent(CapsuleCollider);
+	gameController = GameObject.Find("GameController");
 	
 }
 
@@ -17,16 +26,55 @@ function Update () {
  
  	nav.speed = speed;
  	
- 	Debug.Log(nav.remainingDistance);
+ 	//Debug.Log(nav.remainingDistance);
+ 	
  	
  	if(currentMoves > 0 && nav.pathStatus == NavMeshPathStatus.PathComplete && nav.remainingDistance == 0)
  	{
  		nextPoint = GameObject.Find("Tile" + (currentTile + 1));
-
-	 	nav.destination = nextPoint.transform.position;
+		
+		if(currentMoves != 1)
+		{
+			movementDirection = nextPoint.transform.position;
+		}
+		else
+		{
+			switch(playerNumber)
+			{
+				case 1:
+					movementDirection = Vector3(nextPoint.transform.position.x + offTileDistance, nextPoint.transform.position.y, nextPoint.transform.position.z - offTileDistance);
+				break;
+				case 2: 
+					movementDirection = Vector3(nextPoint.transform.position.x + offTileDistance, nextPoint.transform.position.y, nextPoint.transform.position.z + offTileDistance);
+				break;
+				case 3: 
+					movementDirection = Vector3(nextPoint.transform.position.x - offTileDistance, nextPoint.transform.position.y, nextPoint.transform.position.z + offTileDistance);
+				break;
+				case 4: 
+					movementDirection = Vector3(nextPoint.transform.position.x - offTileDistance, nextPoint.transform.position.y, nextPoint.transform.position.z - offTileDistance);
+				break;
+			}
+			
+			gameController.GetComponent(GameControllerScript).ChangeCurrentPlayer();
+		}
+		
+		nav.destination = movementDirection;
 	 	
  		currentMoves--;
  		currentTile++;
+ 		
+ 		playerIsMoving = true;
+ 	}
+ 	else
+ 	{
+ 		playerIsMoving = false;
+ 		
+ 		
+ 		if(playerNumber == gameController.GetComponent(GameControllerScript).currentPlayerNumber)
+ 		{
+ 			currentMoves = gameController.GetComponent(GameControllerScript).ChooseRandomNumber();
+ 		}
+ 		
  	}
 	
 }
---
+++ ProjetoTurismo/Assets/Scripts/GameControllerScript.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+var randomNum : int = 0;
+var currentPlayerNumber : int = 1;
+var currentPlayer : GameObject;
+
+
+function Start () {
+	currentPlayer = GameObject.Find("Player1");
+	currentPlayer.GetComponent(ChangePositionScript).currentMoves = ChooseRandomNumber();
+	
+	Debug.Log("init player?");
+}
+
+function ChooseRandomNumber() : int{
+	randomNum = Random.Range (1, 6);
+	return randomNum;
+}
+
+function ChangeCurrentPlayer(){
+	
+	if(currentPlayerNumber == 4)
+	{
+		currentPlayerNumber = 1;
+	}
+	else
+	{
+		currentPlayerNumber = currentPlayerNumber + 1;	
+	}
+}
+
+function Update () {
+
+}
