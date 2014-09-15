00e0f8699fd2ad09b253801dcf5c6b8d72ef3586
Andre <andre@lau.eti.br>
2014-09-14 23:00:26 -0300

    Pivot prototype

---
+++ ProjetoTurismo/Assets/Scripts/PivotControllerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PivotControllerScript : MonoBehaviour {
+
+	private Animator anim;
+	private int movingStateHash = Animator.StringToHash("Base Layer.moving");
+	private AnimatorStateInfo stateInfo;
+	public float speed = 0.5f;
+	private float start;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		start = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*float moveH = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+
+		rigidbody.AddForce (new Vector3 (moveV, 0.0f, -moveH) * Time.deltaTime * 20.0f);*/
+		float dTime = Time.time - start;
+		if (dTime > 8) {
+			anim.SetBool ("stopMoving", false);
+		}
+
+		if (dTime > 17) {
+			anim.SetBool ("stopMoving", true);
+		}
+
+		stateInfo = anim.GetCurrentAnimatorStateInfo (0);
+		if (movingStateHash == stateInfo.nameHash) {
+			transform.Translate(-transform.forward * Time.deltaTime * speed);
+		}
+	}
+}
---
+++ ProjetoTurismo/Assets/Scripts/TwisterControllerScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TwisterControllerScript : MonoBehaviour {
+
+	private ParticleSystem.Particle[] particles = new ParticleSystem.Particle[1000];
+	private Vector3 velocity, position;
+	private Vector2 planarVelocity;
+	private int length, i;
+	private float radiusF;
+	public float maxHeight = 1.0f;
+	public float maxRadius = 2.0f;
+	public float baseSpeed = 500;
+	public float speedCoefficient = 1.0f;
+	public float planarAcceleration = 1.0f;
+	public float planarCoefficient = 1.0f;
+	public float verticalAcceleration = 1.0f;
+	public float verticalCoefficient = 1.0f;
+	//public Transform reference;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Update () {
+		length = particleSystem.GetParticles (particles);
+		for (i = 0; i<length; i++) {
+			position = particles[i].position;
+			planarVelocity = new Vector2(position.x, position.y);
+			radiusF = planarVelocity.sqrMagnitude;
+			planarVelocity.Normalize ();
+			planarVelocity = planarVelocity *
+				Mathf.Pow (planarAcceleration * Mathf.Clamp((1.0f - radiusF / maxRadius), 0.0f, 1.0f),
+				planarCoefficient);
+			velocity = new Vector3 (planarVelocity.x, planarVelocity.y,
+				Mathf.Pow(verticalAcceleration * Mathf.Clamp((1.0f - position.z / maxHeight), 0.0f, 1.0f),
+			    verticalCoefficient));
+			position = Quaternion.AngleAxis(Time.deltaTime *
+			Mathf.Pow (baseSpeed * radiusF, speedCoefficient), new Vector3(0.0f, 0.0f, 1.0f)) * position;
+			particles[i].velocity = velocity;
+			particles[i].position = position;
+			
+		}
+		
+		particleSystem.SetParticles (particles, length);
+	}
+
+}
